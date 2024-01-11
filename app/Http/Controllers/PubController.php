9f7836d282516892a95862f889fcfdeecad9e914
Andó Attila <andoattila698@gmail.com>
2024-01-11 16:30:42 +0100

    tr

--- app/Http/Controllers/PubController.php
+++ app/Http/Controllers/PubController.php
@@ -47,4 +47,66 @@ public function getAmountRange(){
     $drinks = DB::table("drinks")->whereBetween("amount",[0,30])->get();
     return $drinks ;
 }
+public function addDrink() {
+
+    DB::table("drinks")->insert([
+        "name"=>"Coca cola",
+        "amount"=>100,
+        "type_id" =>4,
+        "quantity_id" =>1
+    ]);
+    return "OK";
+}
+public function getLastDrinkId(){
+
+    $lastId = DB::table("drinks")->insertGetId([
+        "name"=>"Coca cola",
+        "amount"=>100,
+        "type_id" =>4,
+        "quantity_id" =>1
+    ]);
+    return $lastId;
+}
+public function addMoreDrinks(){
+    DB::table("drinks")->insert([
+        ["name"=>"Jameson",
+        "amount"=>10,
+        "type_id" =>3,
+        "quantity_id" =>1],
+
+["name"=>"Koccintós",
+"amount"=>15,
+"type_id" =>2,
+"quantity_id" =>1]
+]);
+    
+    return "OK";
+}
+public function updateDrink(){
+    DB::table("drinks")->where("id",2)->update([
+        "amount"=>93
+    ]);
+    return "ok";
+}
+public function deleteDrink(){
+    DB::table("drinks")->where("id",12)->delete();
+}
+public function innerJoin(){
+   $drinks = DB::table("drinks")->
+    join("types","types.id", "=","drinks.type_id")->get();
+
+    return $drinks;
+}
+public function leftJoin(){
+ $drinks = DB::table("drinks")->
+ leftJoin("types","drinks.type_id", "=","types.id")->get();
+
+ return $drinks;
+}
+public function rightJoin(){
+    $drinks = DB::table("drinks")->
+    rightjoin("types","drinks.type_id", "=","types.id")->get();
+   
+    return $drinks;
+   }
 }
